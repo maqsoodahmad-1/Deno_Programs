aa3f63c23e72745b4aad8eef80055d3e7379ee2e
maqsoodahmad-1 <maqsoodmaxood75@gmail.com>
2022-07-16 00:25:51 +0530

    Added some basic programs of deno

---
+++ Programming/deps.js
@@ -0,0 +1,2 @@
+export { serve } from 
+ "https://deno.land/std@0.83.0/http/server.ts";
---
+++ Programming/hello-http-server.js
@@ -0,0 +1,5 @@
+export { serve } from 
+   "deps.js";
+    for await (const req of serve(":8080")) {
+        req.respond({body:"Hello deno"});
+    }
